6eb96a0ea0afc7883cf9d3a3695e5f5d4c868067
Eunice <ogwiloeunice@gmail.com>
2026-06-21 06:57:52 +0300

    updated modern UI

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
-});
+})
+function openLightbox(img) {
+  document.getElementById('lightbox').style.display = 'flex';
+  document.getElementById('lightbox-img').src = img.src;
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+}
+const elements = document.querySelectorAll('.fade-in');
+
+function showOnScroll() {
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', showOnScroll);
+showOnScroll();
+</script>
